ffc8d7e8f827892442dd72fa6bfd400e52c83cda
Dicky-Fahriza <dickyfahriza46@gmail.com>
2024-04-23 13:06:12 +0700

    Initial Commit

---
+++ RepublicaNews/RepublicaNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepublicaNewsApp.swift
+//  RepublicaNews
+//
+//  Created by MacBook Pro on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct RepublicaNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
